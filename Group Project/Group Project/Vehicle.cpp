8ae0b871d4f2d19195a24e2034815fadc29d9b81
danielquarrington <48722208+danielquarrington@users.noreply.github.com>
2019-03-19 10:59:43 -0400

    Add files via upload

--- Group Project/Group Project/Vehicle.cpp
+++ Group Project/Group Project/Vehicle.cpp
@@ -1,12 +1,39 @@
 #include "Vehicle.h"
-
+#include <iostream>
 
 
 Vehicle::Vehicle()
 {
+
 }
 
 
 Vehicle::~Vehicle()
 {
+
+}
+
+
+std::istream &operator >>(std::istream &IN, Vehicle &V) {
+	std::cout << "~~~~Vehicle Input~~~~";
+	std::cout << "Year: "; IN >> V.year;
+	std::cout << "Make: "; IN >> V.make;
+	std::cout << "Model: "; IN >> V.model;
+	std::cout << "Vehicle Type"; IN >> V.vehicle_type;
+	std::cout << "Sub Type: "; IN >> V.sub_type;
+	std::cout << "Horsepower: "; IN >> V.horse_power;
+	std::cout << "Fuel Type: "; IN >> V.fuel_type;
+	std::cout << "Rental Cost: "; IN >> V.rental_cost;
+	return IN;
+}
+
+std::ostream &operator <<(std::ostream &OUT, Vehicle &V) {
+	std::cout << "Year: "; OUT << V.year;
+	std::cout << "Make: "; OUT << V.make;
+	std::cout << "Model: "; OUT << V.model;
+	std::cout << "Vehicle Type"; OUT << V.vehicle_type;
+	std::cout << "Sub Type: "; OUT << V.sub_type;
+	std::cout << "Horsepower: "; OUT << V.horse_power;
+	std::cout << "Fuel Type: "; OUT << V.fuel_type;
+	std::cout << "Rental Cost: "; OUT << V.rental_cost;
 }
--- Group Project/Group Project/Vehicle.h
+++ Group Project/Group Project/Vehicle.h
@@ -1,8 +1,39 @@
 #pragma once
+#include <string>
 class Vehicle
 {
+protected:
+	int stock_num, year, horse_power;
+	std::string make, model, vehicle_type, sub_type, fuel_type;
+	double rental_cost;
 public:
 	Vehicle();
 	~Vehicle();
+
+
+	void set_stockNum(int s) { stock_num = s; }
+	void set_year(int y) { year = y; }
+	void set_horsePower(int h) { horse_power = h; }
+	void set_make(std::string m) { make = m; }
+	void set_model(std::string m) { model = m; }
+	void set_vehicleType(std::string v) { vehicle_type = v; }
+	void set_subType(std::string s) { sub_type = s; }
+	void set_fuelType(std::string f) { fuel_type = f; }
+	void set_rentalCost(double r) { rental_cost = r; }
+
+	int get_stockNum() { return stock_num; }
+	int get_year() { return year; }
+	int get_horse_power() { return horse_power; }
+	std::string get_make() { return make; }
+	std::string get_model() { return model; }
+	std::string get_vehicle_type() { return vehicle_type; }
+	std::string get_sub_type() { return sub_type; }
+	std::string get_fuel_type() { return fuel_type; }
+	double get_rental_cost() { return rental_cost; }
+
+	friend std::istream &operator >>(std::istream &, Vehicle &);
+	friend std::ostream &operator <<(std::ostream &, Vehicle &);
+
+
 };
 
